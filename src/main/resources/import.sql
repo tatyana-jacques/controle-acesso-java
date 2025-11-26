75c8a8e6e22adfaca881d719c1dd794408c1ac0f
Tatyana Jacques <taty.aj@gmail.com>
2025-11-26 09:16:42 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$5B1WFBIp6rSkvXD7HMO2n.I4kZWDIRyyoaVKo6tzi7T/vGzm6JHuu');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$5B1WFBIp6rSkvXD7HMO2n.I4kZWDIRyyoaVKo6tzi7T/vGzm6JHuu');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
