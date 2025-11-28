f15e4bc2ed2fbd99a0da5bdc31ad7fe87a2afa0a
Tatyana Jacques <taty.aj@gmail.com>
2025-11-28 10:22:28 -0300

    Spring security checklist, not tested

--- src/main/java/com/devsuperior/demo/entities/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/entities/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.devsuperior.demo.entities;
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +25,16 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
 
+    }
+
+    @Bean
+    @Profile("test")
+    @Order(2)
+    SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        return http.build();
     }
 
 }
